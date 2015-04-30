6a888f23a27c9bbedaa3639f336589977246044a
Atviksord <Henryf@simnet.is>
2015-04-30 19:09:57 +0000

    sql tenging, get og uppfærsla

--- js/sqltenging.php
+++ js/sqltenging.php
@@ -1,7 +1,8 @@
 <?php
 
 $db = new PDO('mysql:host=localhost;dbname=gg', 'root', 'uCGxHXI6LK');
-$stmt = $db->prepare("select word, translation from translation where id=?");
+$stmt = $db->prepare("select word, translation from translation where id=:id");
+$stmt->bindParam(':id', $_GET['id']);
 $stmt->execute($_GET['id']);
 echo json_encode($stmt->fetchAll())
 ?>
